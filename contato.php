182d261f5e79a8896775816a560e27f1d62b4b19
Civalski <100417591+Civalski@users.noreply.github.com>
2024-09-28 15:05:20 -0300

    Add files via upload

---
+++ contato.php
@@ -0,0 +1,37 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Campos do formulário
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $mensagem = $_POST['mensagem'];
+
+    // Verifica o reCAPTCHA
+    $secretKey = '6Lf4YlEqAAAAABpPa88mnDi64eWYQzwcs0xWl4e4';
+    $responseKey = $_POST['g-recaptcha-response'];
+    $userIP = $_SERVER['REMOTE_ADDR'];
+    $reCaptchaURL = "https://www.google.com/recaptcha/api/siteverify?secret=$secretKey&response=$responseKey&remoteip=$userIP";
+
+    $response = file_get_contents($reCaptchaURL);
+    $responseKeys = json_decode($response, true);
+
+    if(intval($responseKeys["success"]) !== 1) {
+        // Falha no reCAPTCHA
+        echo "Por favor, confirme que você não é um robô.";
+    } else {
+        // Configurações do email
+        $to = 'civalskialisson@gmail.com';
+        $subject = 'Nova mensagem do site';
+        $body = "Nome: $nome\nEmail: $email\nMensagem:\n$mensagem";
+        $headers = "From: $email";
+
+        // Envia o email
+        if(mail($to, $subject, $body, $headers)) {
+            echo "Mensagem enviada com sucesso!";
+        } else {
+            echo "Falha ao enviar a mensagem.";
+        }
+    }
+} else {
+    echo "Método inválido.";
+}
+?>
